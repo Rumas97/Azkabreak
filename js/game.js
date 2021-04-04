08465f86ad34af1279b71b9f4e4260ea2a3ca128
Rutul Maskar <rutulmaskar23@gmail.com>
2021-04-04 21:27:16 +0200

    collision condition and movement for harry potter added

--- js/game.js
+++ js/game.js
@@ -20,10 +20,17 @@ spell.src = "./images/spell.jpg";
 let intervalId = 0;
 let isGameOver = false;
 
+let hpX = 30,
+  hpY = 30,
+  hpIncr = 4;
+
+let isArrowUp = false,
+  isArrowDown = false;
+
 // number of dementor objects
 let numOfDem = [
   { x: 845, y: 50 },
-  { x: 845, y: 150 },
+ 
 ];
 
 let distanceBetweenDem = 150;
@@ -38,14 +45,14 @@ function draw() {
   ctx.drawImage(bg, 0, 0);
 
   //adding harry potter image
-  ctx.drawImage(hp, 30, 30);
+  ctx.drawImage(hp, hpX, hpY);
 
   //making dementors move
   for (let i = 0; i < numOfDem.length; i++) {
     ctx.drawImage(dementor, numOfDem[i].x, numOfDem[i].y);
-    //ctx.drawImage(dementor, numOfDem[i].x, numOfDem[i].y + constant )
+    ctx.drawImage(dementor, numOfDem[i].x, numOfDem[i].y + constant )
 
-    numOfDem[i].x = numOfDem[i].x - 5;
+    numOfDem[i].x = numOfDem[i].x - 3;
 
     //y = Math.floor((i*canvas.height/numOfDem.length) +(Math.random()*canvas.height/numOfDem.length)-dementor.height)
 
@@ -59,25 +66,74 @@ function draw() {
         y: Math.floor(Math.random() * (canvas.height - dementor.height)),
       };
     }
+
+    //collsion with dementors
+    if (
+      hpX + hp.width >= numOfDem[i].x &&
+      hpX <= numOfDem[i].x + dementor.width &&
+      (hpY <= numOfDem[i].y + dementor.height ||
+      hpY + hp.height > numOfDem[i].y + constant) 
+    ) {
+      isGameOver = true;
+    }
   }
+
+  if (isArrowUp && hpY > 0) {
+    hpY = hpY - hpIncr;
+  }
+
+  if (isArrowDown && hpY + hp.height < canvas.height) {
+    hpY = hpY + hpIncr;
+  }
+
   if (isGameOver) {
     cancelAnimationFrame(intervalId);
+    gameIntro.style.display = "block";
+    canvas.style.display ='none'
   } else {
     intervalId = requestAnimationFrame(draw);
   }
 }
 
-function start() {
+function start() { //does not work why?
   gameIntro.style.display = "none";
+  console.log(gameIntro.style.display)
+  canvas.style.display = 'block';
   draw();
 }
 
-function restart() {}
+function restart() {
+    isGameOver=false
+    hpX = 30
+    hpY = 30
+    start()
+}
 
 //Event listeners
 
+document.addEventListener("keydown", () => {
+    if (event.code == "ArrowUp") {
+        isArrowUp = true;
+        isArrowDown = false;
+  } else if (event.code == "ArrowDown") {
+        isArrowUp = false;
+        isArrowDown = true;
+  }
+});
+
+document.addEventListener("keyup", () => {
+        isArrowUp = false;
+        isArrowDown = false;
+});
+
 window.addEventListener("load", () => {
-  startBtn.addEventListener("click", () => {
-    start();
-  });
+    startBtn.addEventListener("click", () => {
+        console.log('start')
+        start();
+    });
+    restartBtn.addEventListener("click",() =>{
+        console.log('restart')
+        restart()
+    })
+  
 });
